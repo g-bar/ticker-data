1e36dba6e02228592fa052d18a8c8637ef0bee49
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2024-06-26 15:56:48 +0200

    unneeded env

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,5 @@ import { getEnvVar } from './utils'
 
 const API_KEY = getEnvVar('API_KEY')
 const API_URL = getEnvVar('API_URL')
-const HOME_URL = getEnvVar('NEXT_PUBLIC_HOME_URL')
 
-export { API_KEY, API_URL, HOME_URL }
+export { API_KEY, API_URL }
